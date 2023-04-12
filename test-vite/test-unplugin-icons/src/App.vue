3c078857113fef6305186f2a684b49dc0594822a
wusiquan <1781554387@qq.com>
2023-04-12 17:27:00 +0800

    添加test-vite/test-unplugin-icons

---
+++ test-vite/test-unplugin-icons/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <icon-accessibility /> -->
+  <icon-account-box style="font-size: 2em; color: red" />
+</template>
+
+<script lang="ts">
+import { reactive, defineComponent } from 'vue'
+// import IconAccessibility from '~icons/carbon/accessibility'
+import IconAccountBox from '~icons/mdi/account-box'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    // 'icon-accessibility': IconAccessibility,
+    'icon-account-box': IconAccountBox,
+  },
+
+  setup() {
+    const myState = reactive({ msg: 'hello world' })
+
+    return {
+      myState,
+    }
+  },
+})
+</script>
