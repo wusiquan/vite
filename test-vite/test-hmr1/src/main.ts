e3d087956f136ca104dfb4adf008223426eef5cd
wusiquan <1781554387@qq.com>
2023-09-25 10:46:16 +0800

    add test-hmr1

---
+++ test-vite/test-hmr1/src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+import { sayHello } from './foo'
+
+sayHello()
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
